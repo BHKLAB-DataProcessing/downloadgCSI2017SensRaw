e2a74b820638873446cd8106eba05ff66e784298
imanthonny <34114220+imanthonny@users.noreply.github.com>
2019-08-28 10:02:11 -0400

    Create downloadSensData.R

---
+++ downloadSensData.R
@@ -0,0 +1,16 @@
+library(PharmacoGxPrivate)
+library(devtools)
+
+load("/pfs/gcsi2017raw/sensitivity.RData") #issue with Docker image. This is temp.
+
+
+raw.sensitivity.x <- parallel::splitIndices(nrow(raw.sensitivity), floor(nrow(raw.sensitivity)/1000))
+
+dir.create("/pfs/out/slices/")
+
+for(i in seq_along(raw.sensitivity.x)){
+
+  slce <- raw.sensitivity[raw.sensitivity.x[[i]],,]
+  saveRDS(slce, file=paste0("/pfs/out/slices/gcsi2017_raw_sens_", i, ".rds"))
+
+}
