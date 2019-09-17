ae215760ca04974b11b36cf1f06fd0fd7152ad53
Petr Smirnov <psmirnov2000@gmail.com>
2019-09-17 15:12:38 -0400

    add package installation to script

--- downloadSensData.R
+++ downloadSensData.R
@@ -1,8 +1,59 @@
 library(PharmacoGx)
 library(devtools)
 
-load("/pfs/gcsi2017raw/sensitivity.RData") #issue with Docker image. This is temp.
+install.packages("BiocManager")
+library(BiocManager)
+install(c("multtest", "NMF", "rmarkdown", "RColorBrewer"))
+library(devtools)
 
+install_url("http://research-pub.gene.com/gCSI-cellline-data/compareDrugScreens_current.tar.gz")
+
+# load("/pfs/gcsi2017raw/sensitivity.RData") #issue with Docker image. This is temp.
+
+library(compareDrugScreens)
+
+data("ccle.gcsi.long")
+
+
+library(data.table)
+
+ccle.gcsi.long <- data.table(ccle.gcsi.long)
+setkey(ccle.gcsi.long, CellLine, Drug)
+gcsi.long <- ccle.gcsi.long[Group=="gCSI"]
+
+
+gcsi.long[,Group:=NULL]
+
+gcsi.long[,exp := paste(gcsi.long[,CellLine], gcsi.long[,Drug], sep="_")]
+
+areDuplicates <- duplicated(gcsi.long, by=c("exp", "Dose"))
+
+gcsi.long[areDuplicates, exp:=paste(gcsi.long[areDuplicates, exp], "rep2", sep="_")]  
+
+nconc <- gcsi.long[,.N,.(exp)]
+
+max.con <- max(nconc[,N])
+
+sensitivityRaw <- array(data = NA, dim=c(nrow(nconc), max.con, 2), dimnames=list(nconc[,exp],paste("Dose", 1:max.con), c("Dose", "Viability")))
+
+setkey(gcsi.long, exp, Dose)
+
+gcsi.long <- gcsi.long[order(exp, Dose)]
+
+for (experiment in nconc[,exp]){
+  
+  sensitivityRaw[experiment,1:nconc[exp==experiment,N],] <- as.matrix(gcsi.long[exp==experiment,.(Dose, MedianViability)])
+  
+}
+
+sensitivityRaw[,,"Viability"] <- sensitivityRaw[,,"Viability"]*100
+gcsi.long[,`:=`(cellid = CellLine, drugid = Drug)]
+
+sensitivity.info <- as.data.frame(gcsi.long[,.(unique(drugid),unique(cellid)), by=exp])
+rownames(sensitivity.info) <- sensitivity.info$exp
+colnames(sensitivity.info) <- c("expid", "drugid", "cellid")
+
+save(sensitivity.info, sensitivityRaw, file="/pfs/out/sensitivityRaw.RData")
 
 raw.sensitivity.x <- parallel::splitIndices(nrow(raw.sensitivity), floor(nrow(raw.sensitivity)/1000))
 
